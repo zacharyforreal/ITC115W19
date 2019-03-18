2240ee3b4ec8f12e8269d6c1c0bcc7107b81a951
zacharyforreal <36243126+zacharyforreal@users.noreply.github.com>
2019-03-17 20:30:49 -0700

    Add files via upload

---
+++ Assignment11/Janitor.java
@@ -0,0 +1,20 @@
+
+// A class to represent Janitors.
+public class Janitor extends Employee {
+	public void showSalary() {
+		System.out.println("My salary is $40,000");
+	}
+	
+	public void showHours() {
+		System.out.println("I worked 80 hours per week.");
+	}
+	
+	public void showVacation() {
+		System.out.println("I receive 5 days vacation");
+	}
+	
+    public void clean() {
+        System.out.println("Workin for the man");
+    }
+}
+
