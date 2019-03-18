27768b2522198f3b3f3ff9b0b0a56338d254b757
zacharyforreal <36243126+zacharyforreal@users.noreply.github.com>
2019-03-17 20:43:42 -0700

    Add files via upload

---
+++ Assignment11/HarvardLawyer.java
@@ -0,0 +1,20 @@
+//represents Harvardlawyers
+public class HarvardLawyer  extends Employee{
+
+	public void applyForVacation() {
+		System.out.println("Use the pink vacation form.");
+	}
+
+	public void showVacation() {
+		System.out.println("I receive 24 days vacation");
+	}
+	
+	public void showSalary() {
+		System.out.println("My salary is $120,000");
+		
+	}
+	
+	public void getVacationForm() {
+		System.out.println("pinkpinkpink");
+	}
+}
